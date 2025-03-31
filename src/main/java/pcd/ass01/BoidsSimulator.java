54c7bbe1bf3cf100eb996c51b5bab50e7b1f9443
Riccardo Mazzi - riccardo.mazzi@studio.unibo.it <riccardo.mazzi@studio.unibo.it>
2025-03-31 17:56:37 +0200

    Removed fps limit

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -14,7 +14,7 @@ public class BoidsSimulator {
 
     private BoidsModel model;
     private Optional<BoidsView> view;
-    private static final int FRAMERATE = 25;
+    private static final int FRAMERATE = Integer.MAX_VALUE;
     private int framerate;
     private Barrier barrierVel, barrierSync;
     private final List<UpdateBoids> updateBoidsList = new ArrayList<>();
